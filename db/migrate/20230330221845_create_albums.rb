822f90478ae1e298e9b3bbb0ce0141e0bc5c16b7
Ajaypal Ghuman <asingh0328@yahoo.com>
2023-03-30 18:29:06 -0400

    added colums to album migration

--- db/migrate/20230330221845_create_albums.rb
+++ db/migrate/20230330221845_create_albums.rb
@@ -2,6 +2,10 @@ class CreateAlbums < ActiveRecord::Migration[7.0]
   def change
     create_table :albums do |t|
 
+      t.string :cover
+      t.string :title
+      t.string :artist
+
       t.timestamps
     end
   end
